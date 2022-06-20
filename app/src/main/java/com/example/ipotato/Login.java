d645811fedd01b3a03fa52c7cce14d5789b12e2b
Brenda <brenda.leite1@hotmail.com>
2022-06-20 10:37:51 -0300

    verificacao_login
    Verificação se o campo está vazio e lançar um warning no editText.

--- app/src/main/java/com/example/ipotato/Login.java
+++ app/src/main/java/com/example/ipotato/Login.java
@@ -1,5 +1,6 @@
 package com.example.ipotato;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,10 +9,14 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.text.Html;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +26,7 @@ public class Login extends Fragment implements View.OnClickListener{
     NavController navController;
     Button buttonLogin, buttonRegistrar;
     TextView buttonPopularBanco;
+    EditText editTextNomeUsuario, editTextSenhaUsuario;
 
     //construtor
     public Login() {
@@ -47,6 +53,9 @@ public class Login extends Fragment implements View.OnClickListener{
 
         navController = Navigation.findNavController(view);
 
+        editTextNomeUsuario = view.findViewById(R.id.idEditTextNomeUsuario);
+        editTextSenhaUsuario = view.findViewById(R.id.idEditTextSenhaUsuario);
+
         buttonLogin = view.findViewById(R.id.idButtonLogin);
         buttonLogin.setOnClickListener(this);
 
@@ -62,7 +71,15 @@ public class Login extends Fragment implements View.OnClickListener{
 
         switch (view.getId()){
             case R.id.idButtonLogin:
-                navController.navigate(R.id.action_login_to_iniciarPedido2);
+                if(editTextNomeUsuario.getText().toString().isEmpty() || editTextSenhaUsuario.getText().toString().isEmpty()){
+                    editTextNomeUsuario.setHint("Campo obrigatório!");
+                    editTextNomeUsuario.setHintTextColor(this.getResources().getColor(R.color.vermelho_hint_edittext_verificacao));
+
+                    editTextSenhaUsuario.setHint("Campo obrigatório!");
+                    editTextSenhaUsuario.setHintTextColor(this.getResources().getColor(R.color.vermelho_hint_edittext_verificacao));
+                } else{
+                    navController.navigate(R.id.action_login_to_iniciarPedido3);
+                }
                 break;
 
             case R.id.idButtonRegistrar:
