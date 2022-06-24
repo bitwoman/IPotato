a89e2930787ac498685b0b4f6608bab397e0e5ec
Brenda <brenda.leite1@hotmail.com>
2022-06-24 09:50:13 -0300

    min_alteracoes
    Programei na main de novo - sem querer -. Fiz algumas alterações mas os botões do menu do drawer layout não estão funcionando.

--- app/src/main/java/com/example/ipotato/IniciarPedido.java
+++ app/src/main/java/com/example/ipotato/IniciarPedido.java
@@ -5,26 +5,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.AppBarConfigurationKt;
 
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class IniciarPedido extends Fragment implements View.OnClickListener {
+public class IniciarPedido extends Fragment implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     //Atributos
     NavController navController;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Button buttonIniciarPedido;
+    AppBarConfiguration mAppBarConfiguration;
 
     //Construtor
     public IniciarPedido() {
@@ -47,12 +53,17 @@ public class IniciarPedido extends Fragment implements View.OnClickListener {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //Instância do Navigation Controller, é responsável pelo gerenciamento das tramitações entre os fragmentos
         navController = Navigation.findNavController(view);
+
         drawerLayout = view.findViewById(R.id.drawer_layout);
         navigationView = view.findViewById(R.id.nav_drawer);
+        navigationView.setNavigationItemSelectedListener(this);
 
         buttonIniciarPedido = view.findViewById(R.id.idButtonIniciarPedido);
         buttonIniciarPedido.setOnClickListener(this);
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.cardapio).build();
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 getParentFragment().getActivity(),
@@ -65,8 +76,45 @@ public class IniciarPedido extends Fragment implements View.OnClickListener {
 
     }
 
+    //Evento de clique "padrão" da página de Iniciar Pedido
     @Override
     public void onClick(View view) {
         navController.navigate(R.id.action_iniciarPedido3_to_cardapio);
+    }
+
+    //Evento de clique dos botões presentes no menu do Navigation Drawer
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.idButtonNovoPedido:
+                navController.navigate(R.id.action_iniciarPedido3_to_cardapio);
+                break;
+            case R.id.idButtonHistorico:
+                break;
+            case R.id.idButtonGerenciarGardapio:
+                break;
+            case R.id.idButtonSair:
+                Toast.makeText(getContext(), "Teste filha da puta", Toast.LENGTH_SHORT).show();
+//                finish();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    //Recriei o método de finish, pois o mesmo não é aceito no switch dentro do onNavigationItemSelected
+    public void finish(){
+        finish();
+    }
+
+    //Método responsável pela ação de fechar o menu caso o usuário aperte o botão de "voltar" no Android - ao invés de fechar o App ou voltar para alguma outra aba.
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            onBackPressed();
+        }
     }
 }
