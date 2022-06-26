e05a364d4575b8602bf82c6e999f1645fc21685e
AnaJuliaNevess <annaju245@gmail.com>
2022-06-26 15:40:39 -0300

    Arrumei e comentei as DAO

--- app/src/main/java/com/example/ipotato/MainActivity.java
+++ app/src/main/java/com/example/ipotato/MainActivity.java
@@ -25,6 +25,4 @@ public class MainActivity extends AppCompatActivity {
         uDao.popularTabela();
 
     }
-
-
 }
--- app/src/main/java/com/example/ipotato/dao/ProdutoDAO.java
+++ app/src/main/java/com/example/ipotato/dao/ProdutoDAO.java
@@ -11,15 +11,21 @@ import androidx.annotation.Nullable;
 
 import com.example.ipotato.models.Produto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProdutoDAO extends SQLiteOpenHelper {
 
+    //Nome do banco e versão
     public final static String NOME_BANCO = "bd1";
     public final static int VERSAO_BANCO = 1;
 
+    //Construtor
     public ProdutoDAO(@Nullable Context context) {
         super(context, NOME_BANCO, null, VERSAO_BANCO);
     }
 
+    //On create para executar o SQL que vai criar as tabelas do banco e as colunas.
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE produto(_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -29,26 +35,34 @@ public class ProdutoDAO extends SQLiteOpenHelper {
                 "desconto REAL);");
     }
 
+    //função de atualização do banco caso já exista uma tabela com o nome duplicado
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS produto");
         onCreate(sqLiteDatabase);
     }
 
+    //Função de inserção de registros no banco
     public void inserir(Produto p){
 
+        //criando variável do tipo SQLiteDatabase e chamando o método getWritableDatabase(), funciona para alterações e não somente leitura
         SQLiteDatabase db = this.getWritableDatabase();
 
+        // criando uma instância de ContentValues que é responsável por fazer o get dos campos, no caso inserir os valores nas
+        // respectivas colunas da tabela no banco
         ContentValues insere_valor = new ContentValues();
         insere_valor.put("nome", p.getNome());
         insere_valor.put("preco", p.getPreco());
         insere_valor.put("desconto", p.getDesconto());
 
+        //Chamando minha variavel db e passando a função de inserção no banco, colocando o nome da tabela e passando os valores
+        // coletados através do insere_valores
         db.insert("Produto", null, insere_valor);
 
 //        db.close();
     }
 
+    //Função para atualizar registros no banco
     public void atualizar(String id, Produto p){
         SQLiteDatabase db = this.getWritableDatabase();
 
@@ -59,36 +73,58 @@ public class ProdutoDAO extends SQLiteOpenHelper {
         content.put("preco", p.getPreco());
         content.put("desconto", p.getDesconto());
 
-                                                     //id = vai ser o id que eu to passando por parametro, no caso o id do usuario que esta atualizando
+                                                     //id = vai ser o id que eu to passando por parametro, no caso o id do usuario que
+                                                    //esta atualizando
         db.update("Produto", content,"_id=?", new String[]{id});
 
+        //encerrando a conexão com o banco
         db.close();
     }
 
+    //Função de deleção de registros no banco
     public void deletar(String id){
         SQLiteDatabase db = this.getWritableDatabase();
 
+        //Chamando a função de delete, passando a tabela que eu vou mexer e o id do usuário que eu quero deletar
         db.delete("Produto", "_id=?", new String[]{id});
 
         db.close();
     }
 
-    public void listar(){
+    //Função de listagem de registros no banco
+    public List<Produto> getAllProducts(){
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.query("Produto", new String[] {"_id","nome"}, null, null, null, null, null);
+        //Criando um cursor que vai executar uma Query no meu banco, nessa Query eu passo como parametro a
+        // tabela a qual vou mexer e os campos que eu quero exibir na listagem
+        Cursor cursor = db.query("Produto", new String[] {"_id","nome"},
+                null,
+                null,
+                null,
+                null,
+                null);
 
+        //Se meu cursor acessar o primeiro registro:
+        List<Produto> listP = new ArrayList<>();
         if(cursor.moveToFirst()){
             do {
                 //o getColumnIndexOrThrow está pedindo para eu puxar o valor ou abrir uma exceção na coluna chamada "_id"
                 long id = cursor.getLong(cursor.getColumnIndexOrThrow("_id"));
                 String nome = cursor.getString(cursor.getColumnIndexOrThrow("nome"));
                 double preco = cursor.getDouble(cursor.getColumnIndexOrThrow("preco"));
-                Log.i("Registro: ", id + "" + nome + "" + preco);
+                double desconto = cursor.getDouble(cursor.getColumnIndexOrThrow("desconto"));
+                Produto p = new Produto(
+                        id, nome, preco, desconto
+                );
+                listP.add(p);
+//                Log.i("Registro: ", id + "" + nome + "" + preco);
+            //Enquanto meu cursor se mover para o registro seguinte:
             } while (cursor.moveToNext());
         }
+        //Fecho o cursor e o banco
         cursor.close();
         db.close();
+        return listP;
     }
 
     public void popularBD(){
