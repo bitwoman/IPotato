8d371978f55e29eb27912451331d1cb29cf668e4
Brenda <brenda.leite1@hotmail.com>
2022-06-29 01:32:03 -0300

    organizacao
    Organizei os layout's e as string's no xml, fixei o tamanho das imagens quando forem renderizadas na tela.

--- app/src/main/java/com/example/ipotato/adapters/ProdutoAdapter.java
+++ app/src/main/java/com/example/ipotato/adapters/ProdutoAdapter.java
@@ -1,27 +1,34 @@
 package com.example.ipotato.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.ipotato.R;
+import com.example.ipotato.dao.ProdutoDAO;
 import com.example.ipotato.models.Produto;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ProdutoAdapter extends RecyclerView.Adapter<ProdutoAdapter.ViewHolder>{
 
+    //Atributos
     private Context context;
     private ArrayList<Produto> dataset;
     private OnRecyclerViewItemCliclListener onRecyclerViewItemCliclListener;
+    private Button buttonRemoverQtdeItem, buttonAdicionarQtdeItem, buttonEditarProduto, buttonExcluirProduto;
+    ProdutoDAO tabelaProduto;
+    Produto produto;
 
+    //Construtor
     public ProdutoAdapter(Context context, ArrayList<Produto> dataset) {
         this.context = context;
         this.dataset = dataset;
@@ -57,16 +64,23 @@ public class ProdutoAdapter extends RecyclerView.Adapter<ProdutoAdapter.ViewHold
         private TextView tvNomeProdutoVH;
         private TextView tvPrecoProdutoVH;
         private ImageView ivImagemProdutoVH;
-        private Button buttonRemoverQtdeItem, buttonAdicionarQtdeItem;
+        NavController navController;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            tvNomeProdutoVH = itemView.findViewById(R.id.tvNomeProduto);
-            tvPrecoProdutoVH = itemView.findViewById(R.id.tvPrecoProduto);
-            ivImagemProdutoVH = itemView.findViewById(R.id.ivImagemProduto);
+            tabelaProduto = new ProdutoDAO(context);
+
+            tvNomeProdutoVH = itemView.findViewById(R.id.idTxtViewNomeProdutoPromo);
+            tvPrecoProdutoVH = itemView.findViewById(R.id.idTxtViewPrecoProdutoPromo);
+
+            ivImagemProdutoVH = itemView.findViewById(R.id.idImagemProdutoPromo);
+
             buttonRemoverQtdeItem = itemView.findViewById(R.id.idButtonRemoverQtdeItem);
             buttonAdicionarQtdeItem = itemView.findViewById(R.id.idButtonAdicionarQtdeItem);
+            buttonEditarProduto = itemView.findViewById(R.id.idButtonEditarProduto);
+            buttonExcluirProduto = itemView.findViewById(R.id.idButtonExcluirProduto);
+
             itemView.setOnClickListener(this);
         }
 
@@ -98,6 +112,53 @@ public class ProdutoAdapter extends RecyclerView.Adapter<ProdutoAdapter.ViewHold
         public void onClick(View view) {
             if (onRecyclerViewItemCliclListener != null){
                 onRecyclerViewItemCliclListener.onRecyclerViewItemClick(dataset.get(this.getAdapterPosition()));
+            }
+
+            switch (view.getId()){
+                case R.id.idButtonEditarProduto:
+                    break;
+                case R.id.idButtonExcluirProduto:
+                    //Pra setar os atributos do produto específico, de acordo com o seu id
+//                    produto = dataset.get(i);
+
+                    //Instanciando o objeto da caixa de alerta
+                    AlertDialog.Builder alertDelete = new AlertDialog.Builder(context);
+
+                    //Criando título da janela e a mensagem que será exibida para o usuário
+                    alertDelete.setTitle(R.string.title_alert_dialog);
+                    alertDelete.setMessage(R.string.alert_dialog_warning);
+
+                    //Inflando a caixa de alerta junto aos botões respectivos de confirmar e cancelar
+
+                    //Confirmar
+                    alertDelete.setPositiveButton(R.string.alert_dialog_confirmar, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+//                            tabelaProduto.deletar(produto);
+
+//                            dataset.remove(contato);
+//                            notifyDataSetChanged();
+                        }
+                    });
+
+                    //Cancelar
+                    alertDelete.setNegativeButton(R.string.alert_dialog_cancelar, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+
+                    alertDelete.show();
+
+
+
+
+                    break;
+                case R.id.idButtonAdicionarQtdeItem:
+                    break;
+                case R.id.idButtonRemoverQtdeItem:
+                    break;
             }
         }
     }
